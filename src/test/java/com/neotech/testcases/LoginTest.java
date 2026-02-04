63bcfea58d03fbd6a8f4d43297ed4e52b564d3a9
Erblin Gara <garaerblin@gmail.com>
2026-02-03 21:00:10 -0500

    TestNGFramework

---
+++ src/test/java/com/neotech/testcases/LoginTest.java
@@ -0,0 +1,81 @@
+package com.neotech.testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.neotech.pages.DashboardPageElements;
+import com.neotech.pages.LoginPageElements;
+import com.neotech.utils.CommonMethods;
+import com.neotech.utils.ConfigsReader;
+
+public class LoginTest extends CommonMethods{
+	
+	@Test(groups = {"smoke","regression"})
+	public void validLogin() {
+
+		LoginPageElements loginPage = new LoginPageElements();
+		DashboardPageElements dashboard = new DashboardPageElements();
+
+		// send username
+		sendText(loginPage.username, ConfigsReader.getProperty("username"));
+
+		// send password
+
+		sendText(loginPage.password, ConfigsReader.getProperty("password"));
+
+		// click login button
+		
+		click(loginPage.loginBtn);
+		//or use jsClick() or Actions.click()
+		
+		wait(2);
+		
+		// verify the account name
+		String expected = "Jacqueline White";
+		String actual = dashboard.accountName.getText();
+		
+		//Assertion
+		Assert.assertEquals(actual, expected, "Account does not match");
+		
+	}
+	
+	@Test 
+	public void emptyPassword() {
+		
+		LoginPageElements loginPage = new LoginPageElements();
+		
+		sendText(loginPage.username, ConfigsReader.getProperty("username"));
+		
+		
+		click(loginPage.loginBtn);
+		
+		String expected = "Password cannot be empty";
+		String actual = loginPage.passwordError.getText();
+		
+		Assert.assertEquals(actual, expected, "The error message does not match");
+	}
+	
+	@Test(groups = "regression")
+	public void invalidPassword() {
+		
+		LoginPageElements loginPage = new LoginPageElements();
+		
+		sendText(loginPage.username, ConfigsReader.getProperty("username"));
+		
+		sendText(loginPage.password, "wewqdsa");
+		
+		click(loginPage.loginBtn);
+		
+		
+		String expected = "Invalid Credentials";
+		String actual = loginPage.invalidPassword.getText();
+		
+		Assert.assertEquals(actual, expected, "The error message does not match");
+
+	}
+
+
+}
